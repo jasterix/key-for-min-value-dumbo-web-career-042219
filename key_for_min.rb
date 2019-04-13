0ba50f406c6f17bdd3f3e7ad3fd2dcab787b685a
Jasterix C <loune.calixte@gmail.com>
2019-04-13 01:12:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  smallest_value = nil
+  smallest_name = name
   name_hash.each {|name, number|
     smallest_value = number[0]
     if number < smallest_value
